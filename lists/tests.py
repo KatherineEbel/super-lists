7be76ce4c3e27a18cbd90bc1da249c8979ccefdd
kathyebel <kathyebel@me.com>
2018-08-02 16:53:44 -0400

    FT now handles one list item correctly

--- lists/tests.py
+++ lists/tests.py
@@ -2,10 +2,28 @@ from django.urls import resolve
 from django.test import TestCase
 from lists.views import home_page
 from django.http import HttpRequest
+from django.template.loader import render_to_string
+import re
 
 # Create your tests here.
 
-class HomePageTest(TestCase):
+
+
+class MyTestCase(TestCase):
+
+    @staticmethod
+    def remove_csfr(html_code):
+        csrf_regex = r'<input[^>]+csrfmiddlewaretoken[^>]+>'
+        return re.sub(csrf_regex, '', html_code)
+
+    def assertEqualExceptCSFR(self, html_code1, html_code2):
+        return self.assertEqual(
+            self.remove_csfr(html_code1),
+            self.remove_csfr(html_code2)
+        )
+
+
+class HomePageTest(MyTestCase):
     def test_root_url_resolves_to_home_page_view(self):
         found = resolve('/')
         self.assertEqual(found.func, home_page)
@@ -13,7 +31,24 @@ class HomePageTest(TestCase):
     def test_home_page_returns_correct_html(self):
         request = HttpRequest()
         response = home_page(request)
-        self.assertTrue(response.content.startswith(b'<html>'))
-        self.assertIn(b'<title>To-Do lists</title>', response.content)
-        self.assertTrue(response.content.strip().endswith(b'</html>'))
+        expected_html = render_to_string('home.html')
+        self.assertEqualExceptCSFR(response.content.decode(), expected_html)
+        # self.assertTrue(response.content.startswith(b'<html>'))
+        # self.assertIn(b'<title>To-Do lists</title>', response.content)
+        # self.assertTrue(response.content.strip().endswith(b'</html>'))
+
+    def test_home_page_can_save_a_POST_request(self):
+        request = HttpRequest()
+        request.method = 'POST'
+        request.POST['item_text'] = 'A new list item'
+
+        response = home_page(request)
+        self.assertIn('A new list item', response.content.decode())
+        expected_html = render_to_string(
+            'home.html',
+            {'new_item_text': 'A new list item'},
+            request=request,
+        )
+
+        self.assertEqualExceptCSFR(response.content.decode(), expected_html)
 
