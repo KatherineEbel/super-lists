cd0d952832184fe71ee946da9675c97b35df6b93
kathy <kathyebel@me.com>
2018-08-03 07:46:49 -0400

    added new URL, view, and template to display lists

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -1,22 +1,39 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+from selenium.common.exceptions import WebDriverException
 from django.test import LiveServerTestCase
 import time
+
+WAIT = 1
 
 class NewVisitorTest(LiveServerTestCase):
 
     def setUp(self):
         self.browser = webdriver.Firefox()
-        self.browser.implicitly_wait(3)
+        self.browser.implicitly_wait(WAIT)
 
     def tearDown(self):
         self.browser.quit()
 
     def check_for_row_in_list_table(self, row_text):
-        time.sleep(3)
+        time.sleep(WAIT)
         table = self.browser.find_element_by_id('id_list_table')
         rows = table.find_elements_by_tag_name('tr')
         self.assertIn(row_text, [row.text for row in rows])
+
+
+    def wait_for_row_in_list_table(self, row_text):
+        start_time = time.time()
+        while True:
+            try:
+                table = self.browser.find_element_by_id('id_list_table')
+                rows = table.find_elements_by_tag_name('tr')
+                self.assertIn(row_text, [row.text for row in rows])
+                return
+            except (AssertionError, WebDriverException) as e:
+                if time.time() - start_time > WAIT:
+                    raise e
+                time.sleep(0.5)
 
 
     def test_can_start_a_list_and_retrieve_it_later(self):
@@ -41,8 +58,11 @@ class NewVisitorTest(LiveServerTestCase):
         # When she hits enter, the page updates, and now the page lists
         # "1: Buy peacock feathers" as an item in a to-do list
         inputbox.send_keys(Keys.ENTER)
+
+        time.sleep(WAIT)
         edith_list_url = self.browser.current_url
-        self.assertRegex(edith_list_url, '/lists/.+')
+        print('URL:', edith_list_url)
+        self.assertRegex(edith_list_url, 'lists/.+')
 
         self.check_for_row_in_list_table('1: Buy peacock feathers')
 
@@ -76,8 +96,10 @@ class NewVisitorTest(LiveServerTestCase):
         inputbox.send_keys(Keys.ENTER)
 
         # Francis gets his own unique URL
+        time.sleep(WAIT)
         francis_list_url = self.browser.current_url
-        self.assertRegex(francis_list_url, '/lists/.+')
+        print('Francis_list_url:::::::', francis_list_url)
+        self.assertRegex(francis_list_url, 'lists/.+')
         self.assertNotEqual(francis_list_url, edith_list_url)
 
         # Again, there is no trace of Edith's list
